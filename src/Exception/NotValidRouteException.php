2649affb9be3853944cdca113139aa4b203c5c9c
Maxime <maxime@maximebatista.lycee@gmail.com>
2023-12-02 12:52:21 +0100

    :bookmark: Base exception for router

--- src/Exception/NotValidRouteException.php
+++ src/Exception/NotValidRouteException.php
@@ -2,8 +2,6 @@
 
 namespace Aatis\Routing\Exception;
 
-use Aatis\Routing\Interface\Exception\RouterExceptionInterface;
-
-class NotValidRouteException extends \Exception implements RouterExceptionInterface
+class NotValidRouteException extends RouterException
 {
 }
