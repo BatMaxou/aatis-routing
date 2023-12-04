cd0295df1309a425498d5f0b45fdf73f3681591d
Maxime <maxime@maximebatista.lycee@gmail.com>
2023-12-05 00:20:19 +0100

    :bookmark: Renderer into controller

--- src/Controller/AbstractHomeController.php
+++ src/Controller/AbstractHomeController.php
@@ -3,17 +3,11 @@
 namespace Aatis\Routing\Controller;
 
 use Aatis\Routing\Interface\HomeControllerInterface;
-use Aatis\DependencyInjection\Interface\ContainerInterface;
 
 abstract class AbstractHomeController extends AbstractController implements HomeControllerInterface
 {
-    public function __construct(ContainerInterface $container)
-    {
-        $this->container = $container;
-    }
-
     public function home(): void
     {
-        require_once $_ENV['DOCUMENT_ROOT'].'/../views/pages/home.php';
+        require_once '../vendor/aatis/routing/templates/welcome.tpl.php';
     }
 }
--- src/Exception/NoValidRouteException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Aatis\Routing\Exception;
-
-use Aatis\Routing\Interface\Exception\RouterExceptionInterface;
-
-class NoValidRouteException extends \Exception implements RouterExceptionInterface
-{
-}
