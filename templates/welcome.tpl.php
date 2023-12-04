8a2b1102ebaf397d496bf1a047a9c1378760a0a3
Maxime <maxime@maximebatista.lycee@gmail.com>
2023-12-05 00:15:39 +0100

    :sparkles: Add renderer to controller

---
+++ templates/welcome.tpl.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+</head>
+
+<body>
+    <main>
+        <h1>Welcome to your site !</h1>
+        <h2>Want to start ? Let's go create your first controller.</h2>
+    </main>
+</body>
+
+</html>
